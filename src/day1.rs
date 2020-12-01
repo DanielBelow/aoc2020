dafa20d047576e0d8ce394388f8ca72f11b1a22a
DanielBelow <daniel.below@tum.de>
2020-12-01 12:40:29 +0100

    AOC 2020 initial setup

---
+++ src/day1.rs
@@ -0,0 +1,14 @@
+#[aoc_generator(day1)]
+pub fn generate(inp: &str) -> Vec<u64> {
+    unimplemented!()
+}
+
+#[aoc(day1, part1)]
+pub fn part1(v: &Vec<u64>) -> u64 {
+    unimplemented!()
+}
+
+#[aoc(day1, part2)]
+pub fn part2(v: &Vec<u64>) -> u64 {
+    unimplemented!()
+}
