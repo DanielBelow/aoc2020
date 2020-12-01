8e15222a45f229e55195b5adbf9dca545b966799
DanielBelow <daniel.below@tum.de>
2020-12-01 20:40:07 +0100

    [Day1] Use slices

--- src/day1.rs
+++ src/day1.rs
@@ -8,9 +8,9 @@ pub fn generate(inp: &str) -> Vec<u64> {
     res
 }
 
-fn find_indices(num: u64, v: &Vec<u64>, l: usize, h: usize) -> Option<(usize, usize)> {
-    let mut low = l;
-    let mut high = h;
+fn find_indices(num: u64, v: &[u64]) -> Option<(usize, usize)> {
+    let mut low = 0;
+    let mut high = v.len() - 1;
 
     loop {
         let sum = v[low] + v[high];
@@ -34,7 +34,7 @@ fn find_indices(num: u64, v: &Vec<u64>, l: usize, h: usize) -> Option<(usize, us
 
 #[aoc(day1, part1)]
 pub fn part1(v: &Vec<u64>) -> u64 {
-    let (low, high) = find_indices(2020, v, 0, v.len() - 1).unwrap();
+    let (low, high) = find_indices(2020, v.as_slice()).unwrap();
     v[low] * v[high]
 }
 
@@ -44,11 +44,10 @@ pub fn part2(v: &Vec<u64>) -> u64 {
 
     for i in 0..max_len {
         let curr = v[i];
+        let rem = &v[i + 1..];
 
-        let start = i + 1;
-
-        if let Some((low, high)) = find_indices(2020 - curr, v, start, max_len) {
-            return curr * v[low] * v[high];
+        if let Some((low, high)) = find_indices(2020 - curr, rem) {
+            return curr * rem[low] * rem[high];
         }
     }
 
