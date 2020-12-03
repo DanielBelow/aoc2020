9b9da7ec511d9841eed5f4d9a473e618ec12e42d
DanielBelow <daniel.below@tum.de>
2020-12-03 17:52:16 +0100

    [Day3] Store only coordinates of trees.
    Simplify code.

--- src/day3.rs
+++ src/day3.rs
@@ -1,42 +1,65 @@
+use crate::iterator_ext::IteratorExt;
+
 use aoc_runner_derive::{aoc, aoc_generator};
+use std::collections::HashSet;
+
+pub struct Map {
+    trees: HashSet<(usize, usize)>,
+    width: usize,
+    height: usize,
+}
+
+const TREE: char = '#';
 
 #[aoc_generator(day3)]
-pub fn generate(inp: &str) -> Vec<Vec<bool>> {
-    inp.lines().fold(Vec::new(), |mut acc, line| {
-        let inner = line.chars().fold(Vec::new(), |mut acc, chr| {
-            match chr {
-                '.' | '#' => acc.push(chr == '.'),
-                _ => panic!("Invalid character!"),
-            };
+pub fn generate(inp: &str) -> Map {
+    let height = inp.lines().count();
+    let width = inp.lines().next().unwrap().chars().count();
+
+    let trees = inp
+        .lines()
+        .enumerate()
+        .fold(HashSet::new(), |mut acc, (y, line)| {
+            line.chars()
+                .enumerate()
+                .filter_map(|(idx, chr)| if chr == TREE { Some(idx) } else { None })
+                .for_each(|x| {
+                    acc.insert((x, y));
+                });
 
             acc
         });
 
-        acc.push(inner);
-        acc
-    })
+    Map {
+        trees,
+        width,
+        height,
+    }
 }
 
-fn count_trees_on_slope(right: usize, down: usize, v: &[Vec<bool>]) -> usize {
-    v.iter()
+fn count_trees_on_slope(right: usize, down: usize, m: &Map) -> usize {
+    let width = m.width;
+    let trees = &m.trees;
+
+    (0..=m.height)
         .step_by(down)
         .enumerate()
-        .fold(0, |acc, (idx, inner)| {
-            let res = inner.iter().cycle().step_by(right).nth(idx).unwrap();
-            acc + if *res { 0 } else { 1 }
+        .count_if(|(idx, y)| {
+            let x = (idx * right) % width;
+            trees.contains(&(x, y))
         })
 }
 
 #[aoc(day3, part1)]
-pub fn part1(v: &[Vec<bool>]) -> usize {
-    count_trees_on_slope(3, 1, v)
+pub fn part1(m: &Map) -> usize {
+    count_trees_on_slope(3, 1, m)
 }
 
 #[aoc(day3, part2)]
-pub fn part2(v: &[Vec<bool>]) -> usize {
-    let slopes = vec![(1, 1), (3, 1), (5, 1), (7, 1), (1, 2)];
+pub fn part2(m: &Map) -> usize {
+    const SLOPES: [(usize, usize); 5] = [(1, 1), (3, 1), (5, 1), (7, 1), (1, 2)];
 
-    slopes.iter().fold(1, |acc, &(right, down)| {
-        acc * count_trees_on_slope(right, down, v)
+    SLOPES.iter().fold(1, |acc, &(right, down)| {
+        acc * count_trees_on_slope(right, down, m)
     })
 }
