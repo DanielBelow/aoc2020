5659829dd08451ae11ef1c94935d560e83a029ac
Daniel Below <daniel.below@tum.de>
2020-12-05 20:46:24 +0100

    [Day5] Refactor solution & cleanup

--- src/day5.rs
+++ src/day5.rs
@@ -1,67 +1,51 @@
 use aoc_runner_derive::{aoc, aoc_generator};
 use itertools::Itertools;
+use std::num::ParseIntError;
 
 #[aoc_generator(day5)]
-pub fn generate(inp: &str) -> Vec<i64> {
+pub fn generate(inp: &str) -> Vec<usize> {
     inp.lines()
         .map(String::from)
-        .map(|it| get_seat_id(&it))
+        .filter_map(|it| get_seat_id(&it).map_err(|it| println!("{}", it)).ok())
         .sorted()
         .collect()
 }
 
-fn to_seat_id(row: usize, column: usize) -> i64 {
-    (row * 8 + column) as i64
+fn find_in_range(inp: &str) -> Result<usize, ParseIntError> {
+    let inp = inp
+        .replace('F', "0")
+        .replace('L', "0")
+        .replace('B', "1")
+        .replace('R', "1");
+
+    usize::from_str_radix(&inp, 2)
 }
 
-fn find_in_range(l_bound: usize, u_bound: usize, inp: &str) -> usize {
-    let res = inp.chars().fold(l_bound..=u_bound, |range, it| {
-        let lower = *range.start();
-        let upper = *range.end();
+fn get_seat_id(inp: &str) -> Result<usize, ParseIntError> {
+    let row = find_in_range(&inp[..7])?;
+    let column = find_in_range(&inp[7..])?;
 
-        let half = ((upper - lower) as f32 / 2.0).ceil() as usize;
-        match it {
-            'F' | 'L' => lower..=(upper - half),
-            'B' | 'R' => (lower + half)..=upper,
-            _ => panic!("Invalid character"),
-        }
-    });
-
-    assert_eq!(res.start(), res.end());
-    *res.start()
-}
-
-fn find_seat(inp: &str) -> (usize, usize) {
-    let row = find_in_range(0, 127, &inp[..7]);
-    let column = find_in_range(0, 7, &inp[7..]);
-
-    (row, column)
-}
-
-fn get_seat_id(inp: &str) -> i64 {
-    let (row, col) = find_seat(inp);
-    to_seat_id(row, col)
+    Ok(row * 8 + column)
 }
 
 #[aoc(day5, part1)]
-pub fn part1(boarding_passes: &[i64]) -> Option<i64> {
+pub fn part1(boarding_passes: &[usize]) -> Option<usize> {
     boarding_passes.last().copied()
 }
 
+fn find_missing_seat(boarding_passes: &[usize]) -> Option<usize> {
+    let first_seat = boarding_passes.first();
+    let last_seat = boarding_passes.last();
+
+    let expected_sum = first_seat.and_then(|fs| last_seat.map(|ls| (*fs..=*ls).sum::<usize>()));
+    let actual_sum = boarding_passes.iter().sum::<usize>();
+
+    expected_sum.map(|it| it - actual_sum)
+}
+
 #[aoc(day5, part2)]
-pub fn part2(boarding_passes: &[i64]) -> Option<i64> {
-    boarding_passes
-        .iter()
-        .sorted()
-        .as_slice()
-        .windows(2)
-        .find_map(|it| {
-            if (it[0] - it[1]).abs() == 2 {
-                Some((it[0] + it[1]) / 2)
-            } else {
-                None
-            }
-        })
+pub fn part2(boarding_passes: &[usize]) -> Option<usize> {
+    find_missing_seat(boarding_passes)
 }
 
 #[cfg(test)]
@@ -69,50 +53,15 @@ mod tests {
     use super::*;
 
     #[test]
-    fn test_sample_1() {
-        let inp = "FBFBBFFRLR";
-
-        let (row, col) = find_seat(inp);
-
-        assert_eq!(44, row);
-        assert_eq!(5, col);
-
-        assert_eq!(357, to_seat_id(row, col));
+    fn test_seat_ids() {
+        assert_eq!(357, get_seat_id("FBFBBFFRLR").unwrap());
+        assert_eq!(567, get_seat_id("BFFFBBFRRR").unwrap());
+        assert_eq!(119, get_seat_id("FFFBBBFRRR").unwrap());
+        assert_eq!(820, get_seat_id("BBFFBBFRLL").unwrap());
     }
 
     #[test]
-    fn test_sample_2() {
-        let inp = "BFFFBBFRRR";
-
-        let (row, col) = find_seat(inp);
-
-        assert_eq!(70, row);
-        assert_eq!(7, col);
-
-        assert_eq!(567, to_seat_id(row, col));
-    }
-
-    #[test]
-    fn test_sample_3() {
-        let inp = "FFFBBBFRRR";
-
-        let (row, col) = find_seat(inp);
-
-        assert_eq!(14, row);
-        assert_eq!(7, col);
-
-        assert_eq!(119, to_seat_id(row, col));
-    }
-
-    #[test]
-    fn test_sample_4() {
-        let inp = "BBFFBBFRLL";
-
-        let (row, col) = find_seat(inp);
-
-        assert_eq!(102, row);
-        assert_eq!(4, col);
-
-        assert_eq!(820, to_seat_id(row, col));
+    fn test_missing_int() {
+        assert_eq!(2, find_missing_seat(&[1, 3, 4, 5]).unwrap());
     }
 }
