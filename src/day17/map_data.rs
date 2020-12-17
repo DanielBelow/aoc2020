a9700e51f1563d97ca3e4cf51082df80d5c59840
DanielBelow <daniel.below@tum.de>
2020-12-17 19:58:23 +0100

    [Day17] Add solution

---
+++ src/day17/map_data.rs
@@ -0,0 +1,201 @@
+use crate::iterator_ext::IteratorExt;
+
+pub struct SimulationContext {
+    num_steps: usize,
+}
+
+impl SimulationContext {
+    pub fn new(num_steps: usize) -> Self {
+        Self { num_steps }
+    }
+}
+
+type MapElements3D = Vec<Vec<Vec<bool>>>;
+type MapElements4D = Vec<Vec<Vec<Vec<bool>>>>;
+
+#[derive(Clone)]
+pub struct MapData3d {
+    dimension: usize,
+    elements: MapElements3D,
+}
+
+impl MapData3d {
+    pub fn new(dimension: usize, elements: MapElements3D) -> Self {
+        Self {
+            dimension,
+            elements,
+        }
+    }
+}
+
+#[derive(Clone)]
+pub struct MapData4d {
+    dimension: usize,
+    elements: MapElements4D,
+}
+
+impl MapData4d {
+    pub fn new(dimension: usize, elements: MapElements4D) -> Self {
+        Self {
+            dimension,
+            elements,
+        }
+    }
+}
+
+fn run_iteration_3d(
+    prev_map: &[Vec<Vec<bool>>],
+    start: usize,
+    end: usize,
+    delta: &[(i64, i64, i64)],
+) -> MapElements3D {
+    let mut next_map_elements = prev_map.to_owned();
+
+    for x in start..end {
+        for y in start..end {
+            for z in start..end {
+                let cur_state = prev_map[x][y][z];
+
+                let alive = delta.iter().count_if(|(dx, dy, dz)| {
+                    let x = ((x as i64) + dx) as usize;
+                    let y = ((y as i64) + dy) as usize;
+                    let z = ((z as i64) + dz) as usize;
+
+                    prev_map[x][y][z]
+                });
+
+                if cur_state && alive != 2 && alive != 3 {
+                    next_map_elements[x][y][z] = false;
+                } else if !cur_state && alive == 3 {
+                    next_map_elements[x][y][z] = true;
+                }
+            }
+        }
+    }
+
+    next_map_elements
+}
+
+fn run_iteration_4d(
+    prev_map: &[Vec<Vec<Vec<bool>>>],
+    start: usize,
+    end: usize,
+    delta: &[(i64, i64, i64, i64)],
+) -> MapElements4D {
+    let mut next_map_elements = prev_map.to_owned();
+
+    for x in start..end {
+        for y in start..end {
+            for z in start..end {
+                for w in start..end {
+                    let cur_state = prev_map[x][y][z][w];
+
+                    let alive = delta.iter().count_if(|(dx, dy, dz, dw)| {
+                        let x = ((x as i64) + dx) as usize;
+                        let y = ((y as i64) + dy) as usize;
+                        let z = ((z as i64) + dz) as usize;
+                        let w = ((w as i64) + dw) as usize;
+
+                        prev_map[x][y][z][w]
+                    });
+
+                    if cur_state && alive != 2 && alive != 3 {
+                        next_map_elements[x][y][z][w] = false;
+                    } else if !cur_state && alive == 3 {
+                        next_map_elements[x][y][z][w] = true;
+                    }
+                }
+            }
+        }
+    }
+
+    next_map_elements
+}
+
+fn count_alive_cells(elem: &[Vec<Vec<bool>>]) -> usize {
+    let mut res = 0;
+
+    for x in 0..elem.len() {
+        for y in 0..elem.len() {
+            for z in 0..elem.len() {
+                if elem[x][y][z] {
+                    res += 1;
+                }
+            }
+        }
+    }
+
+    res
+}
+
+pub fn run_simulation_steps_3d(map_data: &MapData3d, context: &SimulationContext) -> usize {
+    let start = 1;
+    let end = map_data.dimension - 2;
+
+    let delta = build_delta_3d();
+
+    let mut state = map_data.elements.to_owned();
+    for _ in 0..context.num_steps {
+        state = run_iteration_3d(&state, start, end, &delta);
+    }
+
+    count_alive_cells(&state)
+}
+
+pub fn run_simulation_steps_4d(map_data: &MapData4d, context: &SimulationContext) -> usize {
+    let start = 1;
+    let end = map_data.dimension - 2;
+
+    let delta = build_delta_4d();
+
+    let mut state = map_data.elements.to_owned();
+    for _ in 0..context.num_steps {
+        state = run_iteration_4d(&state, start, end, &delta);
+    }
+
+    let mut res = 0;
+
+    for elem in state {
+        res += count_alive_cells(&elem);
+    }
+
+    res
+}
+
+fn build_delta_3d() -> Vec<(i64, i64, i64)> {
+    let mut res = Vec::new();
+
+    for x in -1..=1 {
+        for y in -1..=1 {
+            for z in -1..=1 {
+                if x == 0 && y == 0 && z == 0 {
+                    continue;
+                }
+
+                res.push((x, y, z));
+            }
+        }
+    }
+
+    res
+}
+
+fn build_delta_4d() -> Vec<(i64, i64, i64, i64)> {
+    let mut res = Vec::new();
+
+    for x in -1..=1 {
+        for y in -1..=1 {
+            for z in -1..=1 {
+                for w in -1..=1 {
+                    if x == 0 && y == 0 && z == 0 && w == 0 {
+                        continue;
+                    }
+
+                    res.push((x, y, z, w));
+                }
+            }
+        }
+    }
+
+    res
+}
